458746b095ebdba0329843ee9f1d63ab4ea8368a
aycarl <carl.agbenyega@gmail.com>
2022-01-24 21:47:41 -0500

    Updates

---
+++ precache-manifest.144dd1b1fb3080e3487138c3e678d611.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0989f8041c9ef27cfffbc403789f4a43",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6d54b93064fd294885ac",
+    "url": "/static/css/main.af0aa46b.chunk.css"
+  },
+  {
+    "revision": "d52439c465dfb30180fc",
+    "url": "/static/js/2.3d5f3505.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.3d5f3505.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d54b93064fd294885ac",
+    "url": "/static/js/main.268fd6ab.chunk.js"
+  },
+  {
+    "revision": "8f894f2b778ce4107e3b",
+    "url": "/static/js/runtime-main.f9342891.js"
+  },
+  {
+    "revision": "d56c4a10f323f48e3e009e653dceb653",
+    "url": "/static/media/aycarl-11.d56c4a10.png"
+  },
+  {
+    "revision": "3fc43dd2bf1858883ed5e981fe29fc1d",
+    "url": "/static/media/aycarl_color_bg-11.3fc43dd2.png"
+  },
+  {
+    "revision": "4c6b2ecaa114f38ab3b4ec70c2d3863c",
+    "url": "/static/media/behance-logo.4c6b2eca.svg"
+  },
+  {
+    "revision": "74192d2d021e927a58f70811bd23d891",
+    "url": "/static/media/github-logo.74192d2d.svg"
+  },
+  {
+    "revision": "27edb6c7afd1047eddd171d889a4a41b",
+    "url": "/static/media/linkedin-logo.27edb6c7.svg"
+  }
+]);
