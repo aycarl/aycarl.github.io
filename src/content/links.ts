ea58070f6f6893250581e9936b02c443281de2ff
Carl Yao Agbenyega <4999487+aycarl@users.noreply.github.com>
2026-07-15 10:57:59 -0600

    Update CV_URL to new Google Drive link

--- src/content/links.ts
+++ src/content/links.ts
@@ -1,7 +1,7 @@
 // Single source of truth for contact and profile links.
 // Also imported by functions/cv.ts (edge redirect), so keep this module dependency-free.
 
-export const CV_URL = "https://drive.google.com/file/d/14Ofu9R2PmH9hDx0KzmEXxMiQ9_Tl1Od8/view?usp=sharing";
+export const CV_URL = "https://drive.google.com/file/d/1bDmAXbdTT12gg1UFveScoNrTkK-KCVZK/view?usp=sharing";
 
 export const GITHUB_URL = "https://github.com/aycarl";
 export const LINKEDIN_URL = "https://www.linkedin.com/in/aycarl";
